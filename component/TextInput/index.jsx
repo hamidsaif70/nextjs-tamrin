787b384338ab8b0f56c15a3e49f797ec9f8023ea
hamid <hamidsaif70@gmail.com>
2022-05-12 23:05:24 +0430

    salam.test

---
+++ component/TextInput/index.jsx
@@ -0,0 +1,9 @@
+import { css, jsx } from "@emotion/css";
+import Input from "../Input";
+
+const TextInput = ({  placeholder, width }) => {
+    return (
+        <Input type='text' placeholder={placehoder} width={width} ></Input>
+    );
+};
+export default TextInput
